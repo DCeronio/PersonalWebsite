9711d580d3702dc9ce6d4c25817567c650556c24
DCeronio <dylanceronio@sbcglobal.net>
2021-04-20 12:37:45 -0700

    Working on new redesign on website

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,15 +4,18 @@ import '../App.css';
 const Hero = () => {
 
     return ( 
-        <div className="Content Hero">
+        <div className="Content Hero"> 
+            <div id="background"></div>
+            <div id="cover"></div>
             <div className="topNav">
-                <button id="Skills">Skills</button>
-                <button id="Projects">Projects</button>
-                <button id="Contact">Contact</button>
+                <h2 id="Skills">Skills</h2>
+                <h2 id="Projects">Projects</h2>
+                <h2 id="Contact">Contact</h2>
             </div>
             <div className="Main-Title">
                 <h1>DYLAN CERONIO</h1>
-            </div>       
+            </div>    
+            
         </div>
      );
 }
