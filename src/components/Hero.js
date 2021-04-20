54dbfe87398de34056497d362adeaecc8e129dd9
DCeronio <dylanceronio@sbcglobal.net>
2021-04-20 14:53:41 -0700

    initial hero, created svg for background instead of using a css shape

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,23 +1,21 @@
 import '../App.css';
-
+import headshot from '../images/headshot.png';
 
 const Hero = () => {
 
-    return ( 
-        <div className="Content Hero"> 
+    return (
+        <div className="Content Hero">
             <div id="background"></div>
-            <div id="cover"></div>
             <div className="topNav">
                 <h2 id="Skills">Skills</h2>
                 <h2 id="Projects">Projects</h2>
                 <h2 id="Contact">Contact</h2>
             </div>
-            <div className="Main-Title">
-                <h1>DYLAN CERONIO</h1>
-            </div>    
-            
+            <img src={headshot} alt='Professional Headshot' width="242" height="230" />
+            <h1>DYLAN CERONIO</h1>
+            <h2 id="subtitle">Computer Scientist looking for Full-Time Software Engineering Opportunities</h2>
         </div>
-     );
+    );
 }
- 
+
 export default Hero;
