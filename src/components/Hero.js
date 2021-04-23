061f267aef31049b77b80503eab126ca2af683c9
DCeronio <dylanceronio@sbcglobal.net>
2021-04-22 19:33:45 -0700

    dynamic sizing, favicon, hero animation

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,7 @@ import headshot from '../images/headshot.png';
 const Hero = () => {
 
     return (
-        <div className="Content Hero">
+        <div className="Hero">
             <div id="color"></div>
             <div id="background"></div>
             <div className="topNav">
@@ -12,9 +12,11 @@ const Hero = () => {
                 <h2 id="Projects"><a href="#ProjectSection">Projects</a></h2>
                 <h2 id="Contact"><a href="#FooterSection">Contact</a></h2>
             </div>
-            <img src={headshot} alt='Professional Headshot' width="242" height="230" />
-            <h1>DYLAN CERONIO</h1>
-            <h2 id="subtitle">Computer Scientist looking for Full-Time Software Engineering Opportunities</h2>
+            <div className="Titles">
+                <img src={headshot} alt='Professional Headshot' width="242" height="230" />
+                <h1>DYLAN CERONIO</h1>
+                <h2>Computer Scientist looking for Full-Time Software Engineering Opportunities</h2>
+            </div>
         </div>
     );
 }
