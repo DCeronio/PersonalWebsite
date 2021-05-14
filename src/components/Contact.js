10ce1c8beb30960b8755481b22181f5e50dffd0c
DCeronio <dylanceronio@sbcglobal.net>
2021-05-13 20:52:17 -0700

    netlify forms

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ const Contact = () => {
         <>
         <div className="Contact">
             <h1>Send me a message</h1>
-            <form netlify>
+            <form action="POST" data-netlify="true">
                 <input type="text" id="name" name="name" placeholder="Your Name"/>
                 <textarea type="text" id="message" name="message" placeholder="Message"/>
                 <input type="submit" value="Submit"/>
